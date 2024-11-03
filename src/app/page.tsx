a0e5236bfe8fb030a2b556955efaf282e389e4c7
rborges98 <rcborges98@gmail.com>
2024-11-03 13:16:44 -0300

    chore: remove background with tag img and do with background-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,11 @@ import Content from '../components/content'
 
 export default function Home() {
   return (
-    <>
-      <img
-        alt=""
-        className="absolute size-full object-fill"
-        src="images/background.jpg"
-      />
-      <main className="relative z-10 flex min-h-screen w-full flex-col items-center justify-center">
-        <h1 className="font-shadow pb-10 text-center font-athletic-outfit text-8xl text-white">
-          Onde é Hoje?
-        </h1>
-        <Content />
-      </main>
-    </>
+    <main className="relative z-10 flex min-h-screen w-full flex-col items-center justify-center">
+      <h1 className="font-shadow pb-10 text-center font-athletic-outfit text-8xl text-white">
+        Onde é Hoje?
+      </h1>
+      <Content />
+    </main>
   )
 }
